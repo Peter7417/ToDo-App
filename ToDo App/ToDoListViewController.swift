f276dd1e237b1b5ed4df2bbe861c84337b68e152
Partheepan Shiyamsunthar <partheepan_s@live.com>
2020-11-06 11:00:33 +0530

    Added Table View Data Source Methods: 	1. numberOfRowsSelected -> To tabulate the total number of list iems 	2. cellForRoAtindexPath -> To return the name of the array item at the specific table view location

--- ToDo App/ToDoListViewController.swift
+++ ToDo App/ToDoListViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
+    
+    let toDoArray = ["Open Bank Account", "Setup Mobile Plan", "Review Flat details"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    //MARK: - TableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
 }
 
